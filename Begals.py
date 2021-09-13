21d428a51697a6814c2f83d5c2cbab689cf77f85
mikeGLA <mikecommand_conquer@homail.com>
2021-09-13 23:08:31 +0700

    first commit

---
+++ Begals.py
@@ -0,0 +1,28 @@
+#Begals
+import random
+
+answer = random.randint(000,999)
+
+#Create anscheck function
+def answerCheck(guessNum):
+    for guessNum in answer:
+        if guessNum == answer:
+            print("Begals")
+            break
+        elif guessNum != answer:
+            print("Yedmae")
+        else:
+            print("Fermi")
+    return type
+
+#Create tries count
+def guessCount(count):
+    for i in range(10):
+        print("Try number: ", count, "You can guess only 10 times.")
+
+#test
+guessNum = int(input("Enter your guess: "))
+
+Checking = answerCheck(guessNum)
+
+print("Your answer check is: ", Checking)
